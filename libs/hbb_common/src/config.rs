dc093ad9d0f9f68592c942d381fe327a790236af
PP <125181198+PPanaP@users.noreply.github.com>
2024-12-03 15:34:46 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["13b.i234.me"];
-pub const PUBLIC_RS_PUB_KEY: &str = "DsRIejipJMikcMjoWTwlNrO2uUOsOlL+AjNAgWL5i5w=";
+pub const PUBLIC_RS_PUB_KEY: &str = "6123768658";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
